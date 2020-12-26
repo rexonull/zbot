a295650fedd631c3a9a6b3f096d65e3422f877de
rexonull <rexonull@tuta.io>
2020-12-26 22:33:12 +0545

    added comments

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ import os
 from keep_alive import keep_alive
 import functions
 
+# initialize the bot
 client = commands.Bot(command_prefix = "-")
 
 @client.event
 async def on_ready():
     print("Logged in as {0.user}".format(client))
 
+# sends a random quote
 @client.command()
 async def quote(ctx):
     quote = functions.get_quote()
     await ctx.send(quote)
 
+# calculates numbers (only addition, subtracion, multiplication, division and power)
 @client.command()
 async def calc(ctx, num1 = 'x', op = 'x', num2 = 'x'):
     ans = functions.calculate(num1, op, num2)
